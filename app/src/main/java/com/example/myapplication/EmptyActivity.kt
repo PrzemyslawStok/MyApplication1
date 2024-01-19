7bcf895bcaf6d43ae801d90e46cd5f37885c1905
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2024-01-19 18:49:13 +0100

    empty activity

--- app/src/main/java/com/example/myapplication/EmptyActivity.kt
+++ app/src/main/java/com/example/myapplication/EmptyActivity.kt
@@ -2,8 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class EmptyActivity : AppCompatActivity() {
+class EmptyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empty)
