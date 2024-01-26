cfd53f34312a96d7ab4cb313ea926aa37cad81f1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2024-01-26 19:01:29 +0100

    cd ..

--- app/src/main/java/com/example/myapplication/EmptyActivity.kt
+++ app/src/main/java/com/example/myapplication/EmptyActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class EmptyActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empty)
+
+        val layout: ConstraintLayout = findViewById(R.id.constraintLayout)
+        val numberView: TextView = findViewById(R.id.numberView)
+
+        layout.setBackgroundColor(Color.CYAN)
+        numberView.text = "0"
+
     }
 }
